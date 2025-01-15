fe1dbdbfd78492cd4491109bac20b8ae983ade21
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-01-15 09:29:06 +0100

    FIXED HOME

--- public/home.php
+++ public/home.php
@@ -1,29 +1,7 @@
 <?php
 include_once '../utils/autoloader.php';
-session_start();
-
 require '../utils/connect_db.php';
-
-// if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['connexion'])) {
-//     $pseudo = trim($_POST['pseudo']);
-//     if (!empty($pseudo)) {
-//         $stmt = $pdo->prepare("SELECT id FROM user WHERE pseudo = :pseudo");
-//         $stmt->execute(['pseudo' => $pseudo]);
-//         $user = $stmt->fetch();
-
-//         if ($user) {
-//             $_SESSION['user_id'] = $user['id'];
-//         } else {
-//             $stmt = $pdo->prepare("INSERT INTO user (pseudo) VALUES (:pseudo)");
-//             $stmt->execute(['pseudo' => $pseudo]);
-//             $_SESSION['user_id'] = $pdo->lastInsertId();
-//         }
-
-//     } 
-
-//     header("Location: choixquizz.php?id={$_SESSION['user_id']}");
-// }
-
+session_start();
 
 $qcmManager = new QcmManager();
 
