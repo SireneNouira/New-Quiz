4639ff9f1424a9757767ef927522ba723473f39f
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-01-15 09:25:21 +0100

    DONE:PARTIALS+FIXEDCAMELCASE

--- public/home.php
+++ public/home.php
@@ -2,7 +2,7 @@
 include_once '../utils/autoloader.php';
 session_start();
 
- require '../utils/connect_db.php';
+require '../utils/connect_db.php';
 
 // if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['connexion'])) {
 //     $pseudo = trim($_POST['pseudo']);
@@ -27,51 +27,15 @@ session_start();
 
 $qcmManager = new QcmManager();
 
+require_once './partials/header.php'
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz</title>
-    <link rel="stylesheet" href="./assets/styles/style.css">
-</head>
-<body>
-    <header>
-      <div class="logo">
-        <img src="assets/imgs/quizzlogo.png" alt="logo">
-      </div>
-      <nav>
-        <ul>
-          <li><a href="#">A propos</a></li>
-          <li><a href="#">Scores</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
+<main>
+  <section>
+  <?= $qcmManager->generateQcm(1) ?>
+  </section>
+</main>
 
-    <?= $qcmManager->generateQcm(1) ?>
-    
-    <main>
-      <div class="login-container">
-        <div class="logo-container">
-       <img src="assets/imgs/quizzlogo.png" alt="logo" class="logomain">
-        </div>
-     <form class="input-field" method="POST" action="">
-        <label for="pseudo" >Pseudo :</label>
-        <input type="text" name="pseudo" placeholder="PSEUDO..." class="input-field" id="pseudo" required>
-       <button type="submit" name='connexion' class="login-btn" >Se connecter</button>
-    </form>
-        <!-- <input type="text" placeholder="PSEUDO..." class="input-field">
-        <div class="divloginbutton">      
-          <a href="choixquizz.html" class="login-btn">CONNEXION</a>
-        </div>   -->
-      </div>
-    </main>
-  
-    <footer>
-      <div class="footer-text">JOUEZ - APPRENEZ - PROGRESSEZ</div>
-    </footer>
-  </body>
-  </html>
+<?php
+require_once './partials/footer.php'
+?>
